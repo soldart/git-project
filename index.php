ca9823230becf4c98cf3931bf6eda51bc12d20cb
Artem <soldart@mail.ua>
2017-08-25 10:32:12 +0300

    push to the github

--- index.php
+++ index.php
@@ -10,5 +10,6 @@
 	<h3>content</h3>
 	<h2>BRANCH MASTER</h2>
 	<h3>master content</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias laborum numquam esse, assumenda dicta dolorum doloribus. Saepe provident explicabo neque, optio harum nulla deserunt placeat expedita dicta, numquam quo commodi?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam aliquam libero voluptatum, excepturi consequuntur optio ipsa amet id iusto natus eligendi numquam quo odio, dolorem accusamus necessitatibus rem maiores quod.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, iste, impedit? Amet beatae at dolorum esse maiores nesciunt molestiae doloribus. Velit, qui, dolores! Optio, similique quod est provident reprehenderit! Quod.</p>
 </body>
 </html>
